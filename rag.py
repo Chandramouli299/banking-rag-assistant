27a8fa39e086edc92c87490405a874053d2cb833
Chandramouli299 <chandramoulineerukonda@gmail.com>
2026-05-08 10:57:18 +0530

    fixed transformers pipeline

--- rag.py
+++ rag.py
@@ -7,12 +7,13 @@ import os
 from transformers import pipeline
 
 # Load free LLM
+from transformers import pipeline
+
 llm = pipeline(
-    "text2text-generation",
+    task="text2text-generation",
     model="google/flan-t5-base",
     max_new_tokens=120
 )
-
 # -----------------------------
 # Load PDF Documents
 # -----------------------------
